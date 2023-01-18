c50e23eff431f8c112bc4cb626e86930a2a3ab4d
Nguyen Duc Trang <nguyenductrang@Nguyen-Duc-Trang.local>
2023-01-18 13:44:44 +0700

    Update code

--- Gacha/Gacha.sol
+++ Gacha/Gacha.sol
@@ -9,6 +9,7 @@ import "./IERC721Receiver.sol";
 import "./TransferHelper.sol";
 import "./TransferHelper.sol";
 import "./SafeMath.sol";
+import "./IChallenge.sol";
 
 contract Gacha is IERC721Receiver{
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -24,23 +25,32 @@ contract Gacha is IERC721Receiver{
         _;
     }
 
+    // This is a function to add the sender to the admins.
     constructor(){
         admins.add(msg.sender);
     }
 
+    //This is RewardToken struct.
     struct RewardToken{
         address addressToken;
         uint256 totalRate;
         uint256 unlockRate;
         uint256 rawardValue;
         TypeToken typeToken;
+        ChallengeInfo challengeInfo;
     }
 
+    //This is ChallengeInfo struct.
     struct ChallengeInfo{
-        address challengeAddress;
-        ChallengeState _statusChallenge;
+        uint256 targetStepPerDay;
+        uint256 challengeDuration;
+        uint256 stepDataToSend;
+        uint256 minimumAchievementDays;
+        bool isDividendSuccessOrFailure;
+        uint256 amountDeposit;
     }
 
+    //This is UserInfo struct.
     struct UserInfo{
         uint256[] historyDate;
         uint256[] historyData;
@@ -48,27 +58,38 @@ contract Gacha is IERC721Receiver{
         mapping(address => ChallengeInfo) challengeInfo;
     }
 
+    // This is a function to add new reward.
     event AddNewReward(address indexed _addressToken, uint256 _totalRate, uint256 _unlockRate, TypeToken _typeToken);
+    // This is a function to delete reward.
     event DeleteReward(address indexed _addressToken, address _caller);
+    // This is a function to send daily result gacha.
     event SendDailyResultGacha(address indexed _caller, uint256[] _listIndexReward);
-    
-    EnumerableSet.AddressSet private admins;
-    mapping(uint256 => RewardToken) public rewardTokens;
-    mapping(uint256 => UserInfo) public userInfo;
-    Counters.Counter private totalNumberReward;
 
+    EnumerableSet.AddressSet private admins;// This is a function to store the address of admin.
+    mapping(uint256 => RewardToken) public rewardTokens; // This is a mapping to store the reward token.
+    mapping(uint256 => UserInfo) public userInfo; // This is a mapping to store the user info.
+    Counters.Counter private totalNumberReward; // This is a function to count the total number of reward.
+    address public mainNFTAddress; // Declaring a public variable of type address.
+
+    // This function is used to send daily result gacha.
     function sendDailyResultGacha(uint256[] memory _listIndexReward, address _challengerAddress) external returns(bool){
+        // This is a function to check if the list index reward is empty or not.
         if(_listIndexReward.length == 0) {
             return false; 
         }
 
         bool isWonThePrize = false;
+        // This is a loop to check if the reward token is exist or not.
         for(uint256 i = 0 ; i < _listIndexReward.length ; i++) {
+            // This is a function to get the random value.
             uint256 ramdomWithLimitValue = (rewardTokens[_listIndexReward[i]].totalRate).div(
                 rewardTokens[_listIndexReward[i]].unlockRate
             );
+
+            // This is a function to check if the reward token is exist or not.
             if(checkRamdomNumber(ramdomWithLimitValue)) {
                 if(rewardTokens[_listIndexReward[i]].typeToken == TypeToken.ERC20) {
+                    // This is a function to transfer the reward token to the challenger address.
                     TransferHelper.safeTransfer(
                         rewardTokens[_listIndexReward[i]].addressToken,
                         _challengerAddress,
@@ -80,15 +101,23 @@ contract Gacha is IERC721Receiver{
         }
 
         emit SendDailyResultGacha(msg.sender, _listIndexReward);
-    }
 
-    function addNewReward(
+        return isWonThePrize;
+    }
+    
+    // This function is used to add new reward.
+    function updateReward(
         address _addressToken,
         uint256 _totalRate,
         uint256 _unlockRate,
         uint256 _rawardValue,
-        TypeToken _typeToken
+        TypeToken _typeToken,
+        ChallengeInfo memory _challengeInfo
     ) external onlyAdmin{
+        /*
+        This is a function to check if the address token is not zero, 
+        unlock rate is less than total rate and reward value is greater than zero.
+        */
         require(_addressToken != address(0), "ZERO ADDRESS.");
         require(_unlockRate < _totalRate, "UNLOCK RATE MUST BE LESS THAN TOTAL RATE.");
         require(_rawardValue > 0, "VALUE ");
@@ -96,33 +125,72 @@ contract Gacha is IERC721Receiver{
         if(!isRewardTokenExist(_addressToken)){
             totalNumberReward.increment(); 
             uint256 indexOfTokenReward;
+            //This is a loop to check if the reward token is exist or not.
             for(uint256 i = 1; i <= totalNumberReward.current(); i++) {
                 if(rewardTokens[i].addressToken == address(0)) {
                     indexOfTokenReward = i;
                     break;
                 }
             }
-            addOrUpdateReward(indexOfTokenReward, _addressToken, _totalRate, _unlockRate, _rawardValue, _typeToken);
+            // This function is used to add or update reward.
+            addReward(indexOfTokenReward, _addressToken, _totalRate, _unlockRate, _rawardValue, _typeToken, _challengeInfo);
         } else {
-            addOrUpdateReward(findIndexOfTokenReward(_addressToken), _addressToken, _totalRate, _rawardValue, _unlockRate, _typeToken);
+            // This function is used to add or update reward.
+            addReward(findIndexOfTokenReward(_addressToken), _addressToken, _totalRate, _rawardValue, _unlockRate, _typeToken, _challengeInfo);
         }
 
         emit AddNewReward(_addressToken, _totalRate, _unlockRate, _typeToken);
     }
+    
+    function checkRewardConditions(uint256 _indexOfTokenReward, address _challengerAddress) public view returns(bool){
+        uint256 challengeDuration = IChallange(_challengerAddress).duration();
+        if(rewardTokens[_indexOfTokenReward].challengeInfo.targetStepPerDay <= IChallange(_challengerAddress).goal()){
+            if(rewardTokens[_indexOfTokenReward].challengeInfo.challengeDuration <= challengeDuration){
+                uint256[] memory challengeHiostoryData;
+                (, challengeHiostoryData) = IChallange(_challengerAddress).getChallengeHistory();
+                bool isCorrectStepDataToSend = true;
+                for(uint256 i = 0; i < challengeHiostoryData.length; i++) {
+                    if(rewardTokens[_indexOfTokenReward].challengeInfo.stepDataToSend > challengeHiostoryData[i]) {
+                        isCorrectStepDataToSend = false;
+                        break;
+                    }
+                }
+                if(isCorrectStepDataToSend) {
+                    if(challengeDuration > challengeDuration.sub(challengeDuration.div(7))) {
+                        if(rewardTokens[_indexOfTokenReward].challengeInfo.amountDeposit <= IChallange(_challengerAddress).totalReward()) {
+                            address danationAddress = IChallange(mainNFTAddress).donationWalletAddress();
+                            address admin = admins.values()[0];
+                            if(rewardTokens[_indexOfTokenReward].challengeInfo.isDividendSuccessOrFailure) {
+                                if(IChallange(_challengerAddress).getAwardReceiversPercent()[1] == 98) {
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+
+    // This function is used to delete reward.
     function deleteReward(address _addressToken) external {
+        // This function is used to find index of token reward.
         uint256 indexOfTokenReward = findIndexOfTokenReward(_addressToken);
         require(indexOfTokenReward > 0 ,"ADDRESS TOKEN NOT EXIST.");
 
+        // Used to delete the reward token.
         delete rewardTokens[indexOfTokenReward];
 
         emit DeleteReward (_addressToken, msg.sender);
     }
 
+    // This function is used to check if the reward token is exist or not.
     function isRewardTokenExist(address _addressToken) public view returns(bool) {
         if(totalNumberReward.current() == 0) {
             return false;
         } else {
+            // This is a loop to check if the reward token is exist or not.
             for(uint256 i = 1; i <= totalNumberReward.current(); i++) {
                 if(rewardTokens[i].addressToken == _addressToken) {
                     return true;
@@ -132,24 +200,30 @@ contract Gacha is IERC721Receiver{
         return false;
     }
 
-    function addOrUpdateReward(
+    // This function is used to add or update reward.
+    function addReward(
         uint256 _indexOfTokenReward,
         address _addressToken,
         uint256 _totalRate,
         uint256 _unlockRate,
         uint256 _rawardValue,
-        TypeToken _typeToken
+        TypeToken _typeToken,
+        ChallengeInfo memory _challengeInfo
     ) internal {
+        // This is a struct.
         rewardTokens[_indexOfTokenReward] = RewardToken(
             _addressToken,
             _totalRate,
             _unlockRate,
             _rawardValue,
-            _typeToken
+            _typeToken,
+            _challengeInfo
         );
     }
 
+    // This function is used to find index of token reward.
     function findIndexOfTokenReward(address _addressToken) public view returns(uint256 indexOfTokenReward) {
+        // This is a loop to check if the reward token is exist or not.
         for(uint256 i = 1; i <= totalNumberReward.current(); i++) {
             if(rewardTokens[i].addressToken == _addressToken) {
                 indexOfTokenReward = i;
@@ -158,12 +232,15 @@ contract Gacha is IERC721Receiver{
         }
     }
 
+    // This function is used to get total number of reward.
     function getTotalNumberReward() public view returns(uint256) {
         return totalNumberReward.current();
     }
 
+    // This function is used to add or remove admin.
     function updateAdmin(address _adminAddr, bool _flag) external onlyAdmin {
         require(_adminAddr != address(0), "INVALID ADDRESS.");
+        // This is a function to add or remove admin.
         if (_flag) {
             admins.add(_adminAddr);
         } else {
@@ -171,11 +248,14 @@ contract Gacha is IERC721Receiver{
         }
     }
 
+    // This function is used to get all admins.
     function getAdmins() external view returns (address[] memory) {
         return admins.values();
     }
 
+    // This function is used to generate random number.
     function checkRamdomNumber(uint256 _ramdomWithLimitValue) public view returns(bool){
+        // This is a function to generate random number.
         uint256 firstRamdomValue = uint256(
             keccak256(abi.encodePacked(
                 block.number, block.difficulty, msg.sender)
@@ -188,11 +268,17 @@ contract Gacha is IERC721Receiver{
             )
         ) % _ramdomWithLimitValue;
 
+        // This is a function to check if the first random value is equal to the second random value or not.
         if(firstRamdomValue == secondRamdomValue) {
             return true;
         } else {
             return false;
         }
+    }
+
+    function setMainNFTAdress(address _mainNFTAddress) external onlyAdmin{
+        require(_mainNFTAddress != address(0), "INVALID MAIN NFT ADDRESS.");
+        mainNFTAddress = _mainNFTAddress;
     }
 
     /**
